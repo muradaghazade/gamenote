b3d85b81d64f779a487623637e07c26f02ab663d
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-06 20:45:19 +0400

    static files configuration

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView , DetailView , CreateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
