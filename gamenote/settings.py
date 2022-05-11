0961d5ed5115a3472c4d6ad0c4672b9fca1254c7
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-05-12 03:22:11 +0400

    m

--- gamenote/settings.py
+++ gamenote/settings.py
@@ -136,9 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 
